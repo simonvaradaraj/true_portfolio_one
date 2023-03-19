bdc87010f628afb044e6d29dbbd9c8536c90c57b
simonvaradaraj <s.varadaraj03@gmail.com>
2023-03-19 00:30:44 -0500

    starting the projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { SectionWrapper } from '../hoc'
-
+import { motion } from 'framer-motion'
+import { textVariant } from '../utils/motion'
 
 const Projects = () => {
   return (
     <>
-    hi
+      <motion.div variants={textVariant("", "", .2, 1)}>
+        <p className="text-[18px]">திட்டம்</p>
+        <h2 className="font-pp text-[40px]">My Projects</h2>
+      </motion.div>
     </>
   )
 }
